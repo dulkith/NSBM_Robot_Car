393da415aff14ccffb1c37826471127d5d494846
Deshani Vimukthika <32464553+deshaniVimukthika@users.noreply.github.com>
2019-03-26 23:21:15 +0530

    Get distance using Ultrasonic

---
+++ src/ObjectDetector.h
@@ -0,0 +1,14 @@
+
+#ifndef OBJECTDETECTOR_H
+#define OBJECTDETECTOR_H
+
+class ObjectDetector {
+public:
+  ObjectDetector();
+  void SETUP();
+  int distance();
+};
+
+extern ObjectDetector objectDetector;
+
+#endif
