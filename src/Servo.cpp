440c12a3e65545bb3676b02fa61b8048389315e7
Dulkith Bataduea <dulkith@outlook.com>
2019-03-30 22:52:31 +0530

    Servo

---
+++ src/Servo.cpp
@@ -0,0 +1,16 @@
+
+#include <Arduino.h>
+
+#include "Servo.h"
+
+
+Servo::Servo() {}
+
+void Servo::SETUP() {
+
+}
+
+void Servo::rotate() {
+}
+
+Servo servo = Servo();
---
+++ src/Servo.h
@@ -0,0 +1,14 @@
+
+#ifndef SERVO_H
+#define SERVO_H
+
+class Servo {
+public:
+  Servo();
+  void SETUP();
+  void rotate();
+};
+
+extern Servo servo;
+
+#endif
